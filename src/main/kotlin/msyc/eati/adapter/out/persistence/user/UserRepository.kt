3c7d3b1ad22c87fbd21940ddf529bb4e416e1f77
cchhaannn <ychan3397@naver.com>
2025-11-30 14:29:35 +0900

    refactor: 패키지명 변경
    - 코틀린에서 in은 예약어로 사용되어, inbound로 변경

--- src/main/kotlin/msyc/eati/adapter/out/persistence/user/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package msyc.eati.adapter.out.persistence.user
-
-import msyc.eati.domain.user.model.User
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface UserRepository : JpaRepository<User, String>
