f2890cf5b9847a16feda3b4830d58aa4325ea544
cchhaannn <ychan3397@naver.com>
2025-12-24 23:27:14 +0900

    style: favicon 변경

--- src/main/kotlin/msyc/eati/adapter/inbound/web/IndexController.kt
+++ src/main/kotlin/msyc/eati/adapter/inbound/web/IndexController.kt
@@ -1,4 +1,4 @@
-package msyc.eati.adapter.`in`.web
+package msyc.eati.adapter.inbound.web
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
